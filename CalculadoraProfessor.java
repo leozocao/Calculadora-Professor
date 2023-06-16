3fbc1cf2905ef0d869ae30ba0bd985007efaa919
Lucaswpp <105546601+Lucaswpp@users.noreply.github.com>
2023-06-15 21:04:23 -0300

    Sugestão de melhoria.

--- CalculadoraProfessor.java
+++ CalculadoraProfessor.java
@@ -1,7 +1,14 @@
 import java.util.Scanner;
-public class CalculadoraProfessor{
-    public static void main(String[] args) {
+
+
+public class CalculadoraProfessor
+{
+    public static void main(String[] args)
+    {
         Scanner teclado = new Scanner(System.in);
+        double salario_prof = 0;
+
+
         System.out.print("Digite o nome do professor: ");
         String nomeProfessor = teclado.nextLine();
 
@@ -9,32 +16,32 @@ public class CalculadoraProfessor{
         int regimePagamento = teclado.nextInt();
 
         switch (regimePagamento){
-            case 1:
-            System.out.print("Qual o salário mensal do professor? \nR$");
-            double salarioCLT = teclado.nextDouble(); 
 
-            System.out.print("Nome do professor: " + nomeProfessor + "\nValor a receber: R$" + salarioCLT); break;
+            case 1:
+                System.out.print("Qual o salário mensal do professor? \nR$");
+                salario_prof = teclado.nextDouble();
+                break;
 
             case 2:
-            System.out.print("Qual o valor da hora de trabalho? \nR$");
-            double valorHora = teclado.nextDouble();
+                System.out.print("Qual o valor da hora de trabalho? \nR$");
+                double valorHora = teclado.nextDouble();
 
-            System.out.println("Quantas horas são trabalhadas?");
-            double horaTrabalhada = teclado.nextDouble(); 
+                System.out.println("Quantas horas são trabalhadas?");
+                double horaTrabalhada = teclado.nextDouble(); 
 
-            double salarioHorista = valorHora * horaTrabalhada;
-
-            System.out.printf("Nome do professor: %s \nValor a receber: R$%.2f",nomeProfessor,salarioHorista);break;
+                salario_prof = valorHora * horaTrabalhada;
+                break;
             
             case 3:
-            System.out.print("Qual o valor dito no contrato? \nR$");
-            double salarioPJ = teclado.nextDouble(); 
-            
-            System.out.print("Nome do professor: " + nomeProfessor + "\nValor a receber: R$" + salarioPJ); break;
+                System.out.print("Qual o valor dito no contrato? \nR$");
+                salario_prof = teclado.nextDouble(); 
+                break;
             
             default:
-            System.out.print("Tente novamente, utilize uma opção válida.");
-
+                System.out.print("Tente novamente, utilize uma opção válida.");
+                return;
     }
+
+        System.out.print("Nome do professor: " + nomeProfessor + "\nValor a receber: R$" + salario_prof);
     }
 }
